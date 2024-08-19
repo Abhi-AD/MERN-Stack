4a34b92af447a82b4fc8532e8e0f527002b33b6f
Abhi-AD <dangiabhi332@gmail.com>
2024-08-19 15:18:50 +0545

    job filter set

--- JOB PORTAL/frontend/src/container/FilterCard/FilterCard.jsx
+++ JOB PORTAL/frontend/src/container/FilterCard/FilterCard.jsx
@@ -1,5 +1,9 @@
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { setSearchedQuery } from "@/redux/jobSlice";
 
 const fitlerData = [
      {
@@ -17,20 +21,28 @@ const fitlerData = [
 ]
 
 const FilterCard = () => {
-
+     const [selectedValue, setSelectedValue] = useState('');
+     const dispatch = useDispatch();
+     const changeHandler = (value) => {
+          setSelectedValue(value);
+     }
+     useEffect(() => {
+          dispatch(setSearchedQuery(selectedValue));
+     }, [selectedValue]);
      return (
           <div className='w-full  bg-white p-3 rounded-md'>
                <h1 className='font-bold text-lg'>Filter Jobs</h1>
                <hr className='mt-3' />
-               <RadioGroup>
+               <RadioGroup value={selectedValue} onValueChange={changeHandler}>
                     {fitlerData.map((data, index) => (
                          <div key={index}>
                               <h1 className='font-bold text-lg'>{data.fitlerType}</h1>
                               {data.array.map((item, index) => {
+                                   const itemId = `id${index}-${item}`
                                    return (
                                         <div key={index} className='flex items-center space-x-2 my-2'>
-                                             <RadioGroupItem value={item} />
-                                             <Label>{item}</Label>
+                                             <RadioGroupItem value={item} id={itemId} />
+                                             <Label htmlFor={itemId} >{item}</Label>
                                         </div>
                                    )
                               })}
--- JOB PORTAL/frontend/src/routes/Jobs/Jobs.jsx
+++ JOB PORTAL/frontend/src/routes/Jobs/Jobs.jsx
@@ -1,12 +1,25 @@
 import { FilterCard, Job } from "@/container/import";
-import useGetAllJobs from "@/hook/useGetAllJobs";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
-
+import { motion } from "framer-motion";
 
 const Jobs = () => {
-     useGetAllJobs();
+     const { allJobs, searchedQuery } = useSelector(store => store.job);
+     const [filterJobs, setFilterJobs] = useState(allJobs);
 
-     const { allJobs } = useSelector(store => store.job);
+     useEffect(() => {
+          if (searchedQuery) {
+               const filteredJobs = allJobs.filter((job) => {
+                    return job.title.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+                         job.description.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+                         job.location.toLowerCase().includes(searchedQuery.toLowerCase())
+               })
+               setFilterJobs(filteredJobs)
+          } else {
+               setFilterJobs(allJobs)
+          }
+     }, [allJobs, searchedQuery]);
      return (
           <div className="paddingcontainer paddingbuttom">
                <div className="flex gap-5">
@@ -14,15 +27,20 @@ const Jobs = () => {
                          <FilterCard />
                     </div>
                     {
-                         allJobs.length <= 0 ? <span>Job not found</span> : (
+                         filterJobs.length <= 0 ? <span>Job not found</span> : (
                               <div className='flex-1 h-[88vh] overflow-y-auto pb-5'>
                                    <div className='grid grid-cols-3 gap-4'>
                                         {
 
                                              allJobs.map((job) => (
-                                                  <div key={job?._id}>
+                                                  <motion.div
+                                                       initial={{ opacity: 0, x: 100 }}
+                                                       animate={{ opacity: 1, x: 0 }}
+                                                       exit={{ opacity: 0, x: -100 }}
+                                                       transition={{ duration: 0.3 }}
+                                                       key={job?._id}>
                                                        <Job job={job} />
-                                                  </div>
+                                                  </motion.div>
                                              ))
                                         }
                                    </div>
