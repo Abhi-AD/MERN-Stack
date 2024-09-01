4f019b9c00e19af849846098ce1b9f3a099e1cbd
Abhi-AD <dangiabhi332@gmail.com>
2024-09-01 23:20:54 +0545

    Create restaurant API request

--- FOOD DELIVERY/frontend/src/forms/manage-restaurant-form/mern-food-ordering-app-frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx at main · chrisblakely01/ManageRestaurantForm.tsx
+++ FOOD DELIVERY/frontend/src/forms/manage-restaurant-form/mern-food-ordering-app-frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx at main · chrisblakely01/ManageRestaurantForm.tsx
@@ -12,40 +12,41 @@ import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
      restaurantName: z.string({
-          required_error: "restuarant name is required",
+          required_error: "Restaurant name is required",
      }),
      city: z.string({
-          required_error: "city is required",
+          required_error: "City is required",
      }),
      country: z.string({
-          required_error: "country is required",
+          required_error: "Country is required",
      }),
      deliveryPrice: z.coerce.number({
-          required_error: "delivery price is required",
-          invalid_type_error: "must be a valid number",
+          required_error: "Delivery price is required",
+          invalid_type_error: "Must be a valid number",
      }),
      estimatedDeliveryTime: z.coerce.number({
-          required_error: "estimated delivery time is required",
-          invalid_type_error: "must be a valid number",
+          required_error: "Estimated delivery time is required",
+          invalid_type_error: "Must be a valid number",
      }),
      cuisines: z.array(z.string()).nonempty({
-          message: "please select at least one item",
+          message: "Please select at least one item",
      }),
      menuItems: z.array(
           z.object({
-               name: z.string().min(1, "name is required"),
-               price: z.coerce.number().min(1, "price is required"),
+               name: z.string().min(1, "Name is required"),
+               price: z.coerce.number().min(1, "Price is required"),
           })
      ),
-     imageUrl: z.string().optional(),
-})
+     imageFile: z.instanceof(File).optional(), 
+});
 type RestaurantFormData = z.infer<typeof formSchema>;
 
 type Props = {
      onSave: (restaurantFormData: FormData) => void;
      isLoading: boolean;
 };
-const ManageRestaurantForm = ({ onSave, isLoading, restaurant }: Props) => {
+
+const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
      const form = useForm<RestaurantFormData>({
           resolver: zodResolver(formSchema),
           defaultValues: {
@@ -55,7 +56,38 @@ const ManageRestaurantForm = ({ onSave, isLoading, restaurant }: Props) => {
      });
 
      const onSubmit = (formDataJson: RestaurantFormData) => {
+          const formData = new FormData();
+
+          formData.append("restaurantName", formDataJson.restaurantName);
+          formData.append("city", formDataJson.city);
+          formData.append("country", formDataJson.country);
+
+          formData.append(
+               "deliveryPrice",
+               (formDataJson.deliveryPrice * 100).toString()
+          );
+          formData.append(
+               "estimatedDeliveryTime",
+               formDataJson.estimatedDeliveryTime.toString()
+          );
+          formDataJson.cuisines.forEach((cuisine, index) => {
+               formData.append(`cuisines[${index}]`, cuisine);
+          });
+          formDataJson.menuItems.forEach((menuItem, index) => {
+               formData.append(`menuItems[${index}][name]`, menuItem.name);
+               formData.append(
+                    `menuItems[${index}][price]`,
+                    (menuItem.price * 100).toString()
+               );
+          });
+
+          if (formDataJson.imageFile) {
+               formData.append("imageFile", formDataJson.imageFile);
+          }
+
+          onSave(formData);
      };
+
      return (
           <Form {...form}>
                <form
--- FOOD DELIVERY/frontend/src/pages/ManageRestaurantPage.tsx
+++ FOOD DELIVERY/frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,9 +1,14 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/mern-food-ordering-app-frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx at main · chrisblakely01/ManageRestaurantForm"
 
 
 const ManageRestaurantPage = () => {
+     const { createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant();
      return (
-          <ManageRestaurantForm />
+          <ManageRestaurantForm
+               onSave={createRestaurant}
+               isLoading={isCreateLoading}
+          />
      )
 }
 
